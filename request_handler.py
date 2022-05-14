409be642b39e831b3bd6692529c21a00ca70b689
Marcus-Cox <marcus.cox92@gmail.com>
2022-05-14 10:16:37 -0500

    working creat post function

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-
+from views import get_all_posts, get_single_post, create_post, delete_post, update_post
 from views.user import create_user, login_user
 
 
@@ -50,32 +50,89 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        """Handle Get requests to the server"""
-        pass
+        self._set_headers(200)
 
+        response = {}
+
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url()
+
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/posts` or `/posts/2`
+        if len(parsed) == 2:
+            (resource, id) = parsed  # pylint: disable=unbalanced-tuple-unpacking
+
+            if resource == "posts":
+                if id is not None:
+                    response = f"{get_single_post(id)}"
+                else:
+                    response = f"{get_all_posts()}"
+
+        # Response from parse_url() is a tuple with 3
+        # items in it, which means the request was for
+        # `/resource?parameter=value`
+        elif len(parsed) == 3:
+            (resource, key, value) = parsed
+
+        self.wfile.write(response.encode())
 
     def do_POST(self):
         """Make a post request to the server"""
         self._set_headers(201)
         content_len = int(self.headers.get('content-length', 0))
+        print("***" * 50)
+        print(content_len)
+        print("***" * 50)
         post_body = json.loads(self.rfile.read(content_len))
+        print("***" * 50)
+        print(post_body)
+        print("***" * 50)
         response = ''
-        resource, _ = self.parse_url()
-
+        resource, _ = self.parse_url() # pylint: disable=unbalanced-tuple-unpacking
+        print("***" * 50)
+        print(resource)
+        print("***" * 50)
         if resource == 'login':
             response = login_user(post_body)
         if resource == 'register':
             response = create_user(post_body)
+        if resource == 'posts':
+            response = create_post(post_body)
 
         self.wfile.write(response.encode())
 
+
     def do_PUT(self):
-        """Handles PUT requests to the server"""
-        pass
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+    # Parse the URL
+        (resource, id) = self.parse_url()  # pylint: disable=unbalanced-tuple-unpacking
+
+    # Delete a single post from the list
+        if resource == "posts":
+            update_post(id, post_body)
+
+    # Encode the new post and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
-        """Handle DELETE Requests"""
-        pass
+        # Set a 204 response code
+        self._set_headers(204)
+
+    # Parse the URL
+        (resource, id) = self.parse_url(
+        )  # pylint: disable=unbalanced-tuple-unpacking
+
+    # Delete a single post from the list
+        if resource == "posts":
+            delete_post(id)
+
+    # Encode the new post and send in response
+        self.wfile.write("".encode())
 
 
 def main():
